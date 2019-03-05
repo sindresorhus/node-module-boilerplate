e54ededba8a76c8356997b5ce2a7647c8535739d
Edgar Toll <edjopato@gmail.com>
2019-03-05 12:08:56 +0100

    force typescript to allow the bad argument
    otherwise the tsc already rejects this

--- typescript/test/test.ts
+++ typescript/test/test.ts
@@ -3,7 +3,7 @@ import moduleName from '../source';
 
 test('title', t => {
 	t.throws(() => {
-		moduleName(123);
+		(moduleName as any)(123);
 	}, 'Expected a string, got number');
 
 	t.is(moduleName('unicorns'), 'unicorns & rainbows');
