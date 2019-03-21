443b3760e0b99d6ecd385c894b238f959c466200
Edgar Toll <edjopato@gmail.com>
2019-03-21 01:20:10 +0100

    add commonjs default export support

--- typescript/source/index.ts
+++ typescript/source/index.ts
@@ -1,7 +1,11 @@
-export default function (input: string): string {
+export default function moduleName(input: string): string {
 	if (typeof input !== 'string') {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
 	}
 
 	return input + ' & rainbows';
 }
+
+// For CommonJS default export support
+module.exports = moduleName;
+module.exports.default = moduleName;
